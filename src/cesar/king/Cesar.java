1002c1925b80e44d187eb270184a90a1175184c7
Marco <colombaramarco21@gmail.com>
2025-09-23 09:05:30 +0200

    ALL

---
+++ src/cesar/king/Cesar.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cesar.king;
+
+
+/**
+ *
+ * @author 10934253
+ */
+public class Cesar {
+     
+    public Cesar()
+    {
+         
+    }
+}
---
+++ src/cesar/king/CesarKing.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cesar.king;
+import java.util.Scanner
+/**
+ *
+ * @author 10934253
+ */
+public class CesarKing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner idio = new Scanner(System.in);
+        System.out.println("Cifrario - Inserisci chiave : ");
+        int choise = Integer.parseInt(idio.nextLine());
+        do
+        {
+            System.out.println("1)");
+        }while(choise != 0)
+    }
+    
+}
